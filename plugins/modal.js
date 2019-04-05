7688951b61b6ee74b3612579a77091d4213099c9
Ivan Fadeev <githubfadeev@gmail.com>
2019-04-05 21:45:00 +0300

    Created modal base function

---
+++ plugins/modal.js
@@ -0,0 +1,36 @@
+function _createModal(options) {
+    const modal = document.createElement('div');
+    modal.classList.add('winmodal');
+    modal.insertAdjacentHTML('afterbegin', `
+    <div class="overlay">
+            <div class="w-modal">
+                <div class="m-header">
+                    <span class="m-title">Title</span>
+                    <span class="m-close">&times;</span>
+                </div>
+                <div class="m-body">
+                    <p class="m-body__text">Body text 1</p>
+                    <p class="m-body__text">Body text 2</p>
+                </div>
+                <div class="m-footer">
+                    <button>Close</button>
+                    <button>Ok</button>
+
+                </div>
+            </div>
+        </div>
+    `)
+    
+    document.body.appendChild(modal);
+    return modal
+}
+
+$.modal = function(options) {
+    const $modal = _createModal(options)
+
+    return {
+        open() {},
+        close() {},
+        destroy() {}
+    }
+}
